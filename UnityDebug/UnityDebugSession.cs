b31583f359ab0cc9706813a4376af2df107d8421
Lukasz <lukaszunity@users.noreply.github.com>
2015-11-23 12:42:57 +0100

    Added new implementation of UnityDebugSession based upon MonoDebugSession. Create UnityDebugEngine in OpenDebug.EngineFactory.

---
+++ UnityDebug/UnityDebugSession.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Mono.Debugger.Client;
+using OpenDebug;
+
+namespace UnityDebug
+{
+	internal class UnityDebugSession : SDBDebugSession
+	{
+		public UnityDebugSession(Action<DebugEvent> callback) : base(callback)
+		{
+		}
+
+		public override Task<DebugResult> Launch(dynamic args)
+		{
+			return Attach (args);
+		}
+
+		public override Task<DebugResult> Attach(dynamic args)
+		{
+			string name = getString (args, "name");
+			var nameLower = name.ToLower ();
+
+			if (nameLower.Contains ("unity") && nameLower.Contains ("editor")) {
+				int editorPort = FindUnityEditorPort ();
+
+				if (editorPort == -1)
+					return Task.FromResult (new DebugResult (8001, "Could not find Unity editor process", new {}));
+
+				Debugger.Connect (IPAddress.Loopback, editorPort);
+				return Task.FromResult (new DebugResult ());
+			}
+
+			return Task.FromResult (new DebugResult (8001, "Unknown target name '{_name}'. Did you mean 'Unity Editor'?", new { _name = name}));
+		}
+
+		public override Task<DebugResult> Disconnect()
+		{
+			Debugger.Disconnect();
+			return Task.FromResult(new DebugResult());
+		}
+
+		int FindUnityEditorPort()
+		{
+			var processes =  System.Diagnostics.Process.GetProcesses ();
+
+			if (null != processes) {
+				foreach (System.Diagnostics.Process p in processes) {
+					try {
+						if ((p.ProcessName.StartsWith ("unity", StringComparison.OrdinalIgnoreCase) ||
+							p.ProcessName.Contains ("Unity.app")) &&
+							!p.ProcessName.Contains ("UnityShader") &&
+							!p.ProcessName.Contains ("UnityHelper") &&
+							!p.ProcessName.Contains ("Unity Helper")) 
+						{
+							return 56000 + (p.Id % 1000);
+						}
+					} catch {
+						// Don't care; continue
+					}
+				}
+			}
+
+			return -1;
+		}
+
+	}
+}
+
---
+++ UnityDebug/UnityEngineFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using OpenDebug;
+
+namespace OpenDebug
+{
+	public static class EngineFactory
+	{
+		public static IDebugSession CreateDebugSession(string adapterID, Action<DebugEvent> callback)
+		{
+			if (adapterID == "unity")
+				return new UnityDebug.UnityDebugSession (callback);
+
+			return null;
+		}
+	}
+}
