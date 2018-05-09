c030eea73fc9f8d47590db576e52da9255708199
nicklas <miniwolf1508@gmail.com>
2018-05-09 17:53:34 +0200

    Halt on exception works, ui for switching between stage works. No unhandled yet. Doesn't seem doable. Needs to work on adding new exceptions to the list.

---
+++ typescript/attach.ts
@@ -0,0 +1,123 @@
+'use strict';
+
+import {debug, workspace, commands, window, ExtensionContext, QuickPickItem, QuickPickOptions, DebugConfiguration, DebugConfigurationProvider, WorkspaceFolder, CancellationToken, ProviderResult} from 'vscode';
+import {DebugProtocol} from 'vscode-debugprotocol';
+import * as nls from 'vscode-nls';
+import {exec} from 'child_process';
+import { Exceptions, ExceptionConfigurations } from './exceptions';
+
+const localize = nls.config({locale: process.env.VSCODE_NLS_CONFIG})();
+
+const configuration = workspace.getConfiguration('unity-debug');
+var exceptions;
+
+const DEFAULT_EXCEPTIONS: ExceptionConfigurations = {
+    "System.Exception": "never",
+    "System.SystemException": "never",
+    "System.ArithmeticException": "never",
+    "System.ArrayTypeMismatchException": "never",
+    "System.DivideByZeroException": "never",
+    "System.IndexOutOfRangeException": "never",
+    "System.InvalidCastException": "never",
+    "System.NullReferenceException": "never",
+    "System.OutOfMemoryException": "never",
+    "System.OverflowException": "never",
+    "System.StackOverflowException": "never",
+    "System.TypeInitializationException": "never"
+};
+
+export function activate(context: ExtensionContext) {
+    context.subscriptions.push(debug.registerDebugConfigurationProvider("unity", new UnityDebugConfigurationProvider()));
+
+    exceptions = new Exceptions(DEFAULT_EXCEPTIONS);
+    window.registerTreeDataProvider("exceptions", exceptions);
+    context.subscriptions.push(commands.registerCommand('exceptions.always', exception => exceptions.always(exception)));
+    context.subscriptions.push(commands.registerCommand('exceptions.never', exception => exceptions.never(exception)));
+    context.subscriptions.push(commands.registerCommand('exceptions.unhandled', exception => exceptions.unhandled(exception)));
+	context.subscriptions.push(commands.registerCommand('attach.attachToDebugger', config => startSession(context, config)));
+}
+
+export function deactivate() {
+}
+
+class UnityDebugConfigurationProvider implements DebugConfigurationProvider {
+	provideDebugConfigurations(folder: WorkspaceFolder | undefined, token?: CancellationToken): ProviderResult<DebugConfiguration[]> {
+		const config = [
+			{
+				name: "Unity Editor",
+				type: "unity",
+				path: folder.uri.path + "/Library/EditorInstance.json",
+				request: "launch"
+			},
+			{
+				name: "Windows Player",
+				type: "unity",
+				request: "launch"
+			},
+			{
+				name: "OSX Player",
+				type: "unity",
+				request: "launch"
+			},
+			{
+				name: "Linux Player",
+				type: "unity",
+				request: "launch"
+			},
+			{
+				name: "iOS Player",
+				type: "unity",
+				request: "launch"
+			},
+			{
+				name: "Android Player",
+				type: "unity",
+				request: "launch"
+			}
+		];
+		return config;
+	}
+
+	resolveDebugConfiguration(folder: WorkspaceFolder | undefined, debugConfiguration: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
+        if (debugConfiguration && !debugConfiguration.__exceptionOptions) {
+            debugConfiguration.__exceptionOptions = exceptions.convertToExceptionOptionsDefault();
+        }
+		console.log("dimmer resolve");
+		return debugConfiguration;
+	}
+}
+
+function startSession(context: ExtensionContext, config: any) {
+    let execCommand = "";
+    if (process.platform !== 'win32')
+        execCommand = "mono ";
+    exec(execCommand + context.extensionPath + "/bin/UnityDebug.exe list", function (error, stdout, stderr) {
+        const processes = [];
+        const lines = stdout.split("\n");
+        for (let i = 0; i < lines.length; i++) {
+            if (lines[i]) {
+                processes.push(lines[i]);
+            }
+        }
+        if (processes.length == 0) {
+            window.showErrorMessage("No Unity Process Found.");
+        } else {
+            window.showQuickPick(processes).then(function (string) {
+                if (!string) {
+                    return;
+                }
+                config.name    = string;
+                config.request = "launch";
+                config.type    = "unity";
+                config.__exceptionOptions = exceptions.convertToExceptionOptionsDefault();
+                debug.startDebugging(undefined, config)
+                    .then(function (response) {
+                            console.log(response);
+                        },
+                        function (error) {
+                            console.log(error);
+                        });
+            });
+        }
+    });
+}
---
+++ typescript/exceptions.ts
@@ -0,0 +1,121 @@
+import {QuickPickItem, TreeDataProvider, Event, EventEmitter, TreeItem, ProviderResult, window} from 'vscode';
+import * as vscode from 'vscode';
+import * as nls from 'vscode-nls';
+import {DebugProtocol} from 'vscode-debugprotocol';
+
+
+//----- configureExceptions ---------------------------------------------------------------------------------------------------
+// we store the exception configuration in the workspace or user settings as
+export type ExceptionConfigurations = { [exception: string]: DebugProtocol.ExceptionBreakMode; };
+
+class ExceptionItem implements QuickPickItem {
+    label: string;
+    description: string;
+    model: DebugProtocol.ExceptionOptions
+}
+
+export class Exceptions implements TreeDataProvider<Exception> {
+    private _onDidChangeTreeData: EventEmitter<Exception | undefined> = new EventEmitter<Exception | undefined>();
+    readonly onDidChangeTreeData?: Event<Exception | undefined> = this._onDidChangeTreeData.event;
+
+    constructor(private exceptions: ExceptionConfigurations) {
+    }
+
+    public always(element: Exception) {
+        this.exceptions[element.name] = "always";
+        element.mode = "always";
+        this._onDidChangeTreeData.fire();
+        this.setExceptionBreakpoints(this.exceptions);
+    }
+
+    public never(element: Exception) {
+        this.exceptions[element.name] = "never";
+        element.mode = "never";
+        this._onDidChangeTreeData.fire();
+        this.setExceptionBreakpoints(this.exceptions);
+    }
+
+    public unhandled(element: Exception) {
+        this.exceptions[element.name] = "unhandled";
+        element.mode = "unhandled";
+        this._onDidChangeTreeData.fire();
+        this.setExceptionBreakpoints(this.exceptions);
+    }
+
+    setExceptionBreakpoints(model: ExceptionConfigurations) {
+
+        const args: DebugProtocol.SetExceptionBreakpointsArguments = {
+            filters: [],
+            exceptionOptions: this.convertToExceptionOptions(model)
+        };
+
+        vscode.debug.activeDebugSession.customRequest('setExceptionBreakpoints', args).then<DebugProtocol.SetExceptionBreakpointsResponse>();
+    }
+
+    public convertToExceptionOptionsDefault(): DebugProtocol.ExceptionOptions[] {
+        return this.convertToExceptionOptions(this.exceptions);
+    }
+
+    public convertToExceptionOptions(model: ExceptionConfigurations): DebugProtocol.ExceptionOptions[] {
+        const exceptionItems: DebugProtocol.ExceptionOptions[] = [];
+        for (const exception in model) {
+            exceptionItems.push({
+                path: [{names: [exception]}],
+                breakMode: model[exception]
+            });
+        }
+        return exceptionItems;
+    }
+
+    getTreeItem(element: Exception): TreeItem | Thenable<TreeItem> {
+        return element;
+    }
+    getChildren(element?: Exception): ProviderResult<Exception[]> {
+        if (!this.exceptions) {
+            window.showInformationMessage('No exception found');
+            return Promise.resolve([]);
+        }
+
+        return new Promise(resolve => {
+            if (element) {
+                const exceptionItems: Exception[] = [];
+                for (const exception in this.exceptions) {
+                    exceptionItems.push(new Exception(exception, this.exceptions[exception]));
+                }
+                resolve(exceptionItems);
+            } else {
+                const exceptionItems: Exception[] = [];
+                for (const exception in this.exceptions) {
+                    exceptionItems.push(new Exception(exception, this.exceptions[exception]));
+                }
+                resolve(exceptionItems);
+            }
+        });
+    }
+    getParent?(element: Exception): ProviderResult<Exception> {
+        return null;
+    }
+}
+
+class Exception extends TreeItem {
+    constructor(
+        public name: string,
+        public mode: string,
+    ) {
+        super(mode + " : " + name);
+    }
+
+    get label(): string {
+        return `${this.mode} : ${this.name}`;
+    }
+
+    set label(newLabel: string) {
+        this.name = this.name;
+    }
+
+    get tooltip(): string {
+        return `${this.mode} : ${this.name}`;
+    }
+
+    contextValue = 'exception';
+}
